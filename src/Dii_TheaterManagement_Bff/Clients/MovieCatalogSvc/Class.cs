5af6c5dd12e42d757af3657adafdaf815a58b82b
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-09-20 09:23:13 -0500

    Changed long keys to Guid.

---
+++ src/Dii_TheaterManagement_Bff/Clients/MovieCatalogSvc/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dii_TheaterManagement_Bff.Clients.MovieCatalogSvc
+{
+    public class Class
+    {
+    }
+}
